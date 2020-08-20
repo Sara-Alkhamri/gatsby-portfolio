cfeb4c9185d4cd33f5d249f3ec140a338c4796dd
Sara Alkhamri <s.alkhamri@gmail.com>
2020-08-20 16:24:51 -0700

    Created skills.js in components

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,13 @@ import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import Hero from "../components/hero"
+import LatestProjects from "../components/latestProjects"
 
 const IndexPage = ({ data }) => {
   return (
     <Layout>
       <Hero content={data.hero.edges[0].node} />
+      <LatestProjects />
     </Layout>
   )
 }
